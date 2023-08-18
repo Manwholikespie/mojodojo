bba41faeac05c8b9dda222030207e4abb1ee4fb5
Robert Dominguez <rdominguez@me.com>
2023-08-18 10:56:04 -0700

    Update to use HA endpoint

---
+++ lib/mojodojo/home_assistant.ex
@@ -0,0 +1,44 @@
+defmodule Mojodojo.HomeAssistant do
+  @ha_key Application.compile_env(:mojodojo, :ha_key)
+  @ha_endpoint Application.compile_env(:mojodojo, :ha_endpoint)
+
+  defp headers_kw(), do: [{"Authorization", "Bearer #{@ha_key}"}]
+
+  def services() do
+    e = Path.join([@ha_endpoint, "/api/services"])
+    make_request(e, :get)
+  end
+
+  @spec states() :: map()
+  def states() do
+    e = Path.join([@ha_endpoint, "/api/states"])
+    make_request(e, :get)
+  end
+
+  @spec states(bitstring) :: map()
+  def states(entity_id) do
+    e = Path.join([@ha_endpoint, "/api/states", entity_id])
+    make_request(e, :get)
+  end
+
+  def set_domain_service(domain, service, service_data) do
+    e = Path.join([@ha_endpoint, "/api/services", domain, service])
+    make_request(e, :post, service_data)
+  end
+
+  @spec make_request(bitstring(), :get | :post | :put) :: map()
+  defp make_request(endpoint, method, payload \\ nil) do
+    body =
+      case payload do
+        nil -> ""
+        _ -> Jason.encode!(payload)
+      end
+
+    req =
+      Req.Request.new(method: method, url: endpoint, headers: headers_kw(), body: body)
+      |> Req.Request.append_response_steps(decompress_body: fn x -> x end)
+
+    {_req, resp} = Req.Request.run_request(req)
+    Jason.decode!(resp.body)
+  end
+end
--- lib/mojodojo/lights.ex
+++ lib/mojodojo/lights.ex
@@ -1,59 +1,17 @@
 defmodule Mojodojo.Lights do
-  @moduledoc """
-  For managing my smart lights (currently just two light strips connected to a
-    Philips Hue bridge)
-  """
+  alias Mojodojo.HomeAssistant, as: HA
 
-  @spec discover_ip :: %{id: bitstring(), ip: bitstring()}
-  def discover_ip() do
-    %Req.Response{
-      body: [
-        %{
-          "id" => id,
-          "internalipaddress" => ip
-        }
-      ]
-    } = Req.get!("https://discovery.meethue.com")
-
-    %{id: id, ip: ip}
+  def set_rgb(entity_id, r, g, b) do
+    HA.set_domain_service("light", "turn_on", %{
+      "entity_id" => entity_id,
+      "rgb_color" => [r, g, b]
+    })
   end
 
-  @doc """
-  Register your application with the bridge after pressing its link button.
-
-  Example devicetype: "my_hue_app#iphone peter"
-  """
-  @spec get_username(bitstring(), bitstring()) :: bitstring()
-  def get_username(ip, devicetype) do
-    endpoint = "http://#{ip}/api"
-
-    with {:ok, username} <-
-           Req.post!(endpoint, json: %{"devicetype" => devicetype}).body
-           |> helper_process_username() do
-      username
-    end
-  end
-
-  @spec get_lights(bitstring(), bitstring()) :: map()
-  def get_lights(ip, username) do
-    endpoint = helper_endpoint(ip, username) <> "/lights"
-    Req.get!(endpoint).body
-  end
-
-  def update_light(ip, username, light_id, state) do
-    endpoint = helper_endpoint(ip, username) <> "/lights/#{light_id}/state"
-    Req.put!(endpoint, json: state)
-  end
-
-  def helper_endpoint(ip, username), do: "http://#{ip}/api/#{username}"
-
-  def helper_process_username(_request_body)
-
-  def helper_process_username([%{"error" => %{"description" => desc}}]) do
-    {:error, desc}
-  end
-
-  def helper_process_username([%{"success" => %{"username" => username}}]) do
-    {:ok, username}
+  def set_brightness(entity_id, brightness) when brightness >= 0 and brightness <= 255 do
+    HA.set_domain_service("light", "turn_on", %{
+      "entity_id" => entity_id,
+      "brightness" => brightness
+    })
   end
 end
